6cfcfa74ee92a57a062f65cf25b5d65c5ffd8eab
LTDoanh <125759416+LTDoanh@users.noreply.github.com>
2024-11-01 00:00:23 +0700

    Upload Toolbar Fragment and more
    Upload Toolbar Fragment, Settings Activity, Icons, Fonts

---
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_settings);
+        create();
+    }
+
+    private void create() {
+        loadFragment(R.id.fragmentToolbar, new FragmentToolbar());
+    }
+
+    private void loadFragment(int containerId, Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(containerId, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
