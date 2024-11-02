701c2302db29378f8149d62df0179893edcb6fce
Lehoangminh-dab <hoangminh7c12@gmail.com>
2024-11-02 16:01:39 +0700

    feat(LibraryActivity): Added mime types restriction for file picker:

--- app/src/main/java/com/example/myapplication/LibraryActivity.java
+++ app/src/main/java/com/example/myapplication/LibraryActivity.java
@@ -7,7 +7,13 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+
 public class LibraryActivity extends AppCompatActivity {
+    private static final String[] MIME_TYPES = {
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+    };
+    private static final int REQUEST_CODE_UPLOAD_FILE = 1;
     private static final String LOG_TAG = "LibraryActivity";
     private ImageView uploadFileIcon;
 
@@ -25,6 +31,7 @@ public class LibraryActivity extends AppCompatActivity {
 
     private void setupUploadFileIconBehavior () {
         uploadFileIcon.setOnClickListener(v -> {
+            Log.d(LOG_TAG, "Upload file icon clicked");
             openFile();
         });
     }
@@ -33,6 +40,15 @@ public class LibraryActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
-        startActivity(intent);
+        // Restricts the type of files that can be opened to those specified in MIME_TYPES.
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, MIME_TYPES);
+        startActivityForResult(intent, REQUEST_CODE_UPLOAD_FILE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        Log.d(LOG_TAG, "Chosen file URI: " + resultData.getData());
     }
 }
