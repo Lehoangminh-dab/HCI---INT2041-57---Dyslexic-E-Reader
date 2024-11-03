1da68a59ac068c2869db948f405c7b3b92e30691
Lehoangminh-dab <hoangminh7c12@gmail.com>
2024-11-03 21:41:44 +0700

    feat(LibraryActivity): Added support for PNG and JPEG files.

--- app/src/main/java/com/example/myapplication/LibraryActivity.java
+++ app/src/main/java/com/example/myapplication/LibraryActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.utils.textextractor.ImageTextExtractor;
 import com.example.myapplication.utils.textextractor.TextExtractionStrategyFactory;
 import com.example.myapplication.utils.textextractor.TextExtractor;
 import com.google.android.material.snackbar.Snackbar;
@@ -19,6 +20,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class LibraryActivity extends AppCompatActivity {
     private static final String EPUB_MIME_TYPE = "application/epub+zip";
@@ -41,13 +45,16 @@ public class LibraryActivity extends AppCompatActivity {
     private static final String LOG_TAG = "LibraryActivity";
 
     private TextExtractor textExtractor;
+    private ImageTextExtractor imageTextExtractor;
     private ImageView uploadFileIcon;
+    private ExecutorService executorService = Executors.newSingleThreadExecutor(); // Used to run image text extraction on background thread.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
         uploadFileIcon = findViewById(R.id.upload_file_icon);
+        imageTextExtractor = new ImageTextExtractor(this);
         setViewBehaviors();
     }
 
@@ -92,8 +99,7 @@ public class LibraryActivity extends AppCompatActivity {
      *                         These exceptions are handled by the showError() method.
      * @throws IOException if file creation or writing fails (handled internally with logging)
      *
-     * @see #createTextFile(String, String)
-     * @see TextExtractionStrategyFactory#createStrategy(Context, String) 
+     * @see TextExtractionStrategyFactory#createStrategy(Context, String)
      * @see TextExtractor#extractText(Uri)
      * @see android.content.Context#getFilesDir()
      */
@@ -102,8 +108,7 @@ public class LibraryActivity extends AppCompatActivity {
                                  Intent resultData) {
         super.onActivityResult(requestCode, resultCode, resultData);
 
-        // Get fileUri
-        Uri fileUri;
+        // Verify response
         if (requestCode != REQUEST_CODE_UPLOAD_FILE) {
             showMessage("Unexpected request code: " + requestCode);
         }
@@ -114,14 +119,40 @@ public class LibraryActivity extends AppCompatActivity {
             showMessage("Unexpected null result data");
         }
 
-        fileUri = resultData.getData();
+        Uri fileUri = getFileUri(resultData);
         if (fileUri == null) {
             showMessage("Unexpected null file URI");
         }
-
         Log.d(LOG_TAG, "Chosen file URI: " + fileUri);
 
-        // Extract text from file
+        String mimeType = getContentResolver().getType(fileUri);
+        if (mimeType.equals(PNG_MIME_TYPE) || mimeType.equals(JPEG_MIME_TYPE)) {
+            // Extract text from image on a separate thread.
+            extractTextFromImage(fileUri).thenAccept(
+                    extractedText -> {
+                        if (extractedText != null) {
+                            createTextFile(fileUri, extractedText);
+                        } else {
+                            showMessage("Image text content cannot be found.");
+                        }
+                    }
+            );
+        } else {
+            // Extract text from document on the main thread.
+            String extractedText = extractTextFromDocumentFile(fileUri);
+            if (extractedText != null) {
+                createTextFile(fileUri, extractedText);
+            } else {
+                showMessage("Error extracting text from file");
+            }
+        }
+    }
+
+    private Uri getFileUri(Intent data) {
+        return data.getData();
+    }
+
+    private String extractTextFromDocumentFile(Uri fileUri) {
         String mimeType = getContentResolver().getType(fileUri);
         if (!Arrays.asList(MIME_TYPES).contains(mimeType)) {
             showMessage("Unsupported file type: " + mimeType);
@@ -131,24 +162,21 @@ public class LibraryActivity extends AppCompatActivity {
                 mimeType));
         String extractedText = textExtractor.extractText(fileUri);
         Log.d(LOG_TAG, "Extracted text: " + extractedText);
+        return extractedText;
+    }
 
-        // Get chosen file name
-        String fileName = getFileName(fileUri);
-        Log.d(LOG_TAG, "Chosen file name: " + fileName);
+    private CompletableFuture<String> extractTextFromImage(Uri fileUri) {
+        return imageTextExtractor.extractText(fileUri);
+    }
+
+    private void createTextFile(Uri fileUri, String fileContent) {
         // Remove the file name's extension
+        String fileName = getFileName(fileUri);
         String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
-        createTextFile(fileNameWithoutExtension + ".txt", extractedText);
-        // Notify upload success to user
-        showMessage("File uploaded successfully!");
-    }
+        String textFileName = fileNameWithoutExtension + ".txt";
 
-    private void showMessage(String message) {
-        Snackbar.make(uploadFileIcon, message, Snackbar.LENGTH_LONG).show();
-    }
-
-    private void createTextFile(String fileName, String fileContent) {
-        File file = new File(this.getFilesDir(), fileName);
-
+        // Create the file with content in the app's private storage directory
+        File file = new File(this.getFilesDir(), textFileName);
         try {
             if (!file.exists()) {
                 file.createNewFile();
@@ -161,7 +189,10 @@ public class LibraryActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        showMessage("File uploaded successfully!");
     }
+
 
     private String getFileName(Uri fileUri) {
         Cursor returnCursor = getContentResolver().query(fileUri, null, null,
@@ -173,5 +204,9 @@ public class LibraryActivity extends AppCompatActivity {
 
         int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
         return returnCursor.getString(nameIndex);
+    }
+
+    private void showMessage(String message) {
+        Snackbar.make(uploadFileIcon, message, Snackbar.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/myapplication/utils/textextractor/TextExtractionStrategyFactory.java
+++ app/src/main/java/com/example/myapplication/utils/textextractor/TextExtractionStrategyFactory.java
@@ -25,8 +25,6 @@ public class TextExtractionStrategyFactory {
                 return new EpubTextExtractionStrategy(context);
             case PDF_MIME_TYPE:
                 return new PdfTextExtractionStrategy(context);
-            case PNG_MIME_TYPE:
-                return new PngTextExtractionStrategy(context);
         }
 
         Log.e(LOG_TAG, "Unsupported mime type: " + mimeType);
