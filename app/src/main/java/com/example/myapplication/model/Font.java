6394d278268b409f10a15cf5f170c6a7b9046515
LTDoanh <125759416+LTDoanh@users.noreply.github.com>
2024-11-05 17:32:52 +0700

    Update Font.java

--- app/src/main/java/com/example/myapplication/model/Font.java
+++ app/src/main/java/com/example/myapplication/model/Font.java
@@ -9,64 +9,6 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 
-public class Font implements SharedPreferences, Serializable {
+public class Font implements Serializable {
 
-
-
-    @Override
-    public Map<String, ?> getAll() {
-        return Collections.emptyMap();
-    }
-
-    @Nullable
-    @Override
-    public String getString(String key, @Nullable String defValue) {
-        return "";
-    }
-
-    @Nullable
-    @Override
-    public Set<String> getStringSet(String key, @Nullable Set<String> defValues) {
-        return Collections.emptySet();
-    }
-
-    @Override
-    public int getInt(String key, int defValue) {
-        return 0;
-    }
-
-    @Override
-    public long getLong(String key, long defValue) {
-        return 0;
-    }
-
-    @Override
-    public float getFloat(String key, float defValue) {
-        return 0;
-    }
-
-    @Override
-    public boolean getBoolean(String key, boolean defValue) {
-        return false;
-    }
-
-    @Override
-    public boolean contains(String key) {
-        return false;
-    }
-
-    @Override
-    public Editor edit() {
-        return null;
-    }
-
-    @Override
-    public void registerOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
-
-    }
-
-    @Override
-    public void unregisterOnSharedPreferenceChangeListener(OnSharedPreferenceChangeListener listener) {
-
-    }
 }
